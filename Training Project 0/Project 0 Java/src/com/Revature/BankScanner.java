300836efe50b40d8a459c1aba99ba61a91b68009
Anna <mounkeygirl@yahoo.com>
2020-10-20 10:32:37 -0500

    Basic Start
    Started Sepereate Scanner Class

---
+++ Training Project 0/Project 0 Java/src/com/Revature/BankScanner.java
@@ -0,0 +1,5 @@
+package com.Revature;
+
+public static class BankScanner {
+
+}
---
+++ Training Project 0/Project 0 Java/src/com/Revature/Main.java
@@ -0,0 +1,21 @@
+package com.Revature;
+
+import java.util.Scanner;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String name;
+		
+		
+		System.out.println("Please enter your name:");
+		name=scan.nextLine();
+		
+		System.out.println("Hello "+name);
+
+	}
+
+}
